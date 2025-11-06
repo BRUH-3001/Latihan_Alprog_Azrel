2c03a1c8eb4c5b13495419d130ef7f89321c178d
BRUH-3001 <azrelsakhireswara2007@gmail.com>
2025-11-06 21:59:12 +0700

    Third Push

---
+++ Percobaan17.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n, temp;
+
+    cin >> n;
+
+    string nama[n];
+    for(int i = 0; i < n; i++){
+        cin >> nama[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < nama[i].size(); j++) {
+            int temp = j + 1;
+            if (nama[i][j] == 'c' || nama[i][j] == 'C') {
+                if (j == nama[i].size() - 1)
+                    nama[i][j] = 'k';
+                else if (nama[i][temp] == 'a' ||
+                         nama[i][temp] == 'u' ||
+                         nama[i][temp] == 'o')
+                    nama[i][j] = 'k';
+                else if (nama[i][temp] == 'h') {
+                    nama[i][temp] = 'c';
+                    j++;
+                }
+                else if (nama[i][temp] == 'e' || nama[i][temp] == 'i' ||
+                         nama[i][temp] == 'y')
+                    nama[i][j] = 's';
+                else
+                    nama[i][j] = 'k';
+            }
+            cout << nama[i][j];
+        }
+        cout << endl;
+    }
+    return 0;
+}
